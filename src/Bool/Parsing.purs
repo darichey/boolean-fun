3978e3462cd6d54ff7eb2ac2886bb5ba0d1dd947
David Richey <darichey1@gmail.com>
2021-03-15 12:42:26 -0500

    Skip whitespace when parsing

--- src/Bool/Parsing.purs
+++ src/Bool/Parsing.purs
@@ -6,11 +6,11 @@ import Bool.Model (BoolExpr(..))
 import Control.Alt ((<|>))
 import Control.Lazy (fix)
 import Text.Parsing.StringParser (Parser)
-import Text.Parsing.StringParser.CodePoints (anyLetter, char, string)
+import Text.Parsing.StringParser.CodePoints as CP
 import Text.Parsing.StringParser.Expr (Assoc(..), Operator(..), buildExprParser)
 
-boolExpr :: Parser BoolExpr
-boolExpr = fix $ \p -> buildExprParser opTable (boolTerm p)
+boolExpr' :: Parser BoolExpr
+boolExpr' = fix $ \p -> buildExprParser opTable (boolTerm p)
   where
   opTable = [ [ Prefix (string "~" $> Not) ]
             , [ Infix (string "*" $> And) AssocRight ]
@@ -19,3 +19,20 @@ boolExpr = fix $ \p -> buildExprParser opTable (boolTerm p)
 
   boolTerm p = (Var <$> anyLetter)
             <|> char '(' *> p <* char ')'
+
+boolExpr :: Parser BoolExpr
+boolExpr = CP.skipSpaces *> lexeme boolExpr'
+
+-- Utilities for dealing with whitespace
+
+lexeme :: forall a. Parser a -> Parser a
+lexeme p = p <* CP.skipSpaces
+
+string :: String -> Parser String
+string = lexeme <<< CP.string
+
+anyLetter :: Parser Char
+anyLetter = lexeme CP.anyLetter
+
+char :: Char -> Parser Char
+char = lexeme <<< CP.char
