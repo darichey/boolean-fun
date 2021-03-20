a1e18ced7f1c99e3a7c93987825dba4610ac2cab
David Richey <darichey1@gmail.com>
2021-03-19 20:53:45 -0500

    Refactor tests

--- test/Bool/ParsingSpec.purs
+++ test/Bool/ParsingSpec.purs
@@ -3,53 +3,79 @@ module Test.Spec.ParsingSpec where
 import Prelude
 
 import Bool.Model (BoolExpr(..))
-import Bool.Parsing (BoolNotation, algebraicNotation, boolExpr, programmingNotation)
+import Bool.Parsing (algebraicNotation, boolExpr, programmingNotation)
+import Control.Monad.Error.Class (class MonadThrow)
 import Data.Either (Either(..))
+import Data.Traversable (traverse_)
+import Effect.Exception (Error)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Text.Parsing.StringParser (runParser)
+import Text.Parsing.StringParser (ParseError, runParser)
+
+type TestConf a m =
+  { name :: String 
+  , input :: { algebraic :: String, programming :: String }
+  , expected :: a -> m Unit
+  }
+
+tests :: forall m. MonadThrow Error m => Array (TestConf (Either ParseError BoolExpr) m)
+tests =
+  [ { name: "parses a single variable"
+    , input:
+        { algebraic: "a"
+        , programming: "a"
+        }
+    , expected: (_ `shouldEqual` (Right (Var 'a')))
+    }
+  , { name: "parses a NOT expression"
+    , input:
+        { algebraic: "~a"
+        , programming: "!a"
+        }
+    , expected: (_ `shouldEqual` (Right (Not (Var 'a'))))
+    }
+  , { name: "parses an AND expression"
+    , input:
+        { algebraic: "a * b"
+        , programming: "a && b"
+        }
+    , expected: (_ `shouldEqual` (Right (And (Var 'a') (Var 'b'))))
+    }
+  , { name: "parses an OR expression"
+    , input:
+        { algebraic: "a + b"
+        , programming: "a || b"
+        }
+    , expected: (_ `shouldEqual` (Right (Or (Var 'a') (Var 'b'))))
+    }
+  , { name: "parses a complex expression"
+    , input:
+        { algebraic: "(a + (~b * c)) * ~d"
+        , programming: "(a || (!b && c)) && !d"
+        }
+    , expected: (_ `shouldEqual` (Right (And (Or (Var 'a') (And (Not (Var 'b')) (Var 'c'))) (Not (Var 'd')))))
+    }
+  , { name: "parses with extraneous whitespace"
+    , input:
+        { algebraic: "   (a \t  + ( ~ b   *\n\n  c)     ) * ~d    "
+        , programming: "   (a \t  || ( ! b   &&\n\n  c)     ) && !d    "
+        }
+    , expected: (_ `shouldEqual` (Right (And (Or (Var 'a') (And (Not (Var 'b')) (Var 'c'))) (Not (Var 'd')))))
+    }
+  ]
 
 spec :: Spec Unit
 spec =
   describe "ParsingSpec" do
-    describe "algebraic notation" do
-      it "parses a single variable" do
-        let input = "a"
-        let res = runParser (boolExpr algebraicNotation) input
-        res `shouldEqual` (Right (Var 'a'))
+    doTests "algebraic notation" algebraicNotation _.algebraic
+    doTests "programming notation" programmingNotation _.programming
 
-      it "parses a NOT expression" do
-        let input = "~a"
-        let res = runParser (boolExpr algebraicNotation) input
-        res `shouldEqual` (Right (Not (Var 'a')))
+    where
+    doTest notation getInput test =
+      it test.name do
+        let res = runParser (boolExpr notation) (getInput test.input)
+        test.expected res
 
-      it "parses an AND expression" do
-        let input = "a * b"
-        let res = runParser (boolExpr algebraicNotation) input
-        res `shouldEqual` (Right (And (Var 'a') (Var 'b')))
-
-      it "parses an OR expression" do
-        let input = "a + b"
-        let res = runParser (boolExpr algebraicNotation) input
-        res `shouldEqual` (Right (Or (Var 'a') (Var 'b')))
-
-    describe "programming notation" do
-      it "parses a single variable" do
-        let input = "a"
-        let res = runParser (boolExpr programmingNotation) input
-        res `shouldEqual` (Right (Var 'a'))
-
-      it "parses a NOT expression" do
-        let input = "!a"
-        let res = runParser (boolExpr programmingNotation) input
-        res `shouldEqual` (Right (Not (Var 'a')))
-
-      it "parses an AND expression" do
-        let input = "a && b"
-        let res = runParser (boolExpr programmingNotation) input
-        res `shouldEqual` (Right (And (Var 'a') (Var 'b')))
-
-      it "parses an OR expression" do
-        let input = "a || b"
-        let res = runParser (boolExpr programmingNotation) input
-        res `shouldEqual` (Right (Or (Var 'a') (Var 'b')))
+    doTests name notation getInput =
+      describe name do
+        traverse_ (doTest notation getInput) tests
